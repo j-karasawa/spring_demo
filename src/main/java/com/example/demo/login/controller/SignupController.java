e8de1ccb503510a9e3feb57d37459c6271d1c5e7
j.karasawa <kara.joe.524@outlook.jp>
2024-05-23 23:14:58 +0900

    データバインドの実装

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,8 +5,12 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.SignupForm;
 
 
 @Controller
@@ -25,7 +29,7 @@ public class SignupController {
     } 
 
     @GetMapping("/signup")
-    public String getSignUp(Model model) {
+    public String getSignUp(@ModelAttribute SignupForm form, Model model) {
 
         radioMarriage = initRadioMarrige();
 
@@ -35,7 +39,12 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignUp(Model model) {
+    public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return getSignUp(form, model);
+        }
+
+        System.out.println(form);
         
         return "redirect:/login";
     }
