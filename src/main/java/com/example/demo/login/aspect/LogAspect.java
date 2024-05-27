7f64ec26d3f3c41231e442d46b1865463cdfa7b1
j.karasawa <kara.joe.524@outlook.jp>
2024-05-27 22:48:26 +0900

    AOP実装

---
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -0,0 +1,34 @@
+package com.example.demo.login.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+    
+    @Around("bean(* Controller)")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("メソッド開始：" + jp.getSignature());
+
+        try {
+            Object result = jp.proceed();
+
+            System.out.println("メソッド終了:" +jp.getSignature());
+
+            return result;
+        } catch(Exception e) {
+            System.err.println("メソッド異常終了:" +jp.getSignature());
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    // @After("execution(* *..*.*Controller.*(..))")
+    // public void endLog(JoinPoint jp) {
+    //     System.out.println("メソッド終了:" + jp.getSignature());
+    // }
+
+}
